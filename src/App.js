e224be4ca400dd88c006a5b8310a8338570eba70
Esra Cebi <esracebi.81@gmail.com>
2021-10-16 17:12:32 +0300

    componentler oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import { ChatEngine} from 'react-chat-engine';
 import './App.css';
+import  ChatFeed  from './components/ChatFeed';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="
+            ba81f5b4-04a7-45cb-a31c-6199e4d748ee"
+            userName="esracebi"
+            userSecret="1234"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps }/>}
+        />
+    )
+}
+
+export default App;
